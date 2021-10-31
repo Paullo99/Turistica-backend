dab92ddab8e66440ed61e49e4ec7ee09a7b9e86b
Paweł Maciończyk <macionczyk.paw@gmail.com>
2021-10-31 12:52:26 +0100

    initialize project

---
+++ src/main/java/pl/turistica/TuristicaBackendApplication.java
@@ -0,0 +1,13 @@
+package pl.turistica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TuristicaBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TuristicaBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/turistica/TuristicaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.turistica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TuristicaBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
